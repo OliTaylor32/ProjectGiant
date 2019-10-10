0f2d1780e2a30f921e2fab5554d8e9320abbdc4d
Oli <oli.taylor.2001@gmail.com>
2019-10-10 12:29:43 +0100

    Visual changes and villager movement
    Added a model to the giant to make it clearer what it will pick up. Basic villager movement which needs more work, but works for prototyping purposes

--- Project Giant/Assets/Scripts/Object.cs
+++ Project Giant/Assets/Scripts/Object.cs
@@ -6,10 +6,16 @@ public class Object : MonoBehaviour
 {
     public string item;
     public int weight;
+    public Transform townCenter;
+    public int speed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        if(item == "Villiger")
+        {
+            StartCoroutine(Move());
+        }
         
     }
 
@@ -23,5 +29,19 @@ public class Object : MonoBehaviour
     {
         sender.SendMessage("ReturnWeight", weight, SendMessageOptions.DontRequireReceiver);
         print("Sent Weight");
+    }
+
+    private IEnumerator Move()
+    {
+        Vector3 target = new Vector3((townCenter.position.x + Random.Range(-5, 5)), townCenter.position.y, (townCenter.position.z + Random.Range(-5, 5)));
+        transform.LookAt(target);
+        while (Vector3.Distance(transform.position, target) > 1)
+        {
+            //print("step");
+            transform.position = Vector3.MoveTowards(transform.position, target, ((speed * 0.1f) * Time.deltaTime));
+            yield return new WaitForSeconds(0.01f);
+        }
+        StartCoroutine(Move());
+
     }
 }
