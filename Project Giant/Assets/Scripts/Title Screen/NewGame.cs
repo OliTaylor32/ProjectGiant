35373add5ec7a0b744c164e50bc72f61628055f6
Oli <oli.taylor.2001@gmail.com>
2019-11-12 14:34:52 +0000

    Title screen
    Added and implemented a title screen

---
+++ Project Giant/Assets/Scripts/Title Screen/NewGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGame : MonoBehaviour
+{
+    public string function;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        print("clicked");
+        if (function == "newgame")
+        {
+            #pragma warning disable CS0618 // Type or member is obsolete
+            Application.LoadLevel("PlayArea");
+            #pragma warning restore CS0618 // Type or member is obsolete
+        }
+        if (function == "quit")
+        {
+            Application.Quit();
+        }
+    }
+}
