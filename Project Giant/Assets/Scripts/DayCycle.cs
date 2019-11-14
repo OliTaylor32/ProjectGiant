03b1923cdd256548a052e9b7d10f5d1c583336ab
Oli <oli.taylor.2001@gmail.com>
2019-11-14 22:32:17 +0000

    Day end
    After 10 mins the player is returned to the title screen

---
+++ Project Giant/Assets/Scripts/DayCycle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour
+{
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - timer >= 600)
+        {
+            #pragma warning disable CS0618 // Type or member is obsolete
+            Application.LoadLevel("TitleScreen");
+            #pragma warning restore CS0618 // Type or member is obsolete
+        }
+    }
+}
