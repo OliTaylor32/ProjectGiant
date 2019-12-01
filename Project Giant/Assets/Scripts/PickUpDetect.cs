09867c9c12be7b42b3fae437342ee7802f68865c
Oli <oli.taylor.2001@gmail.com>
2019-12-01 13:53:38 +0000

    Overhaul on picking up
    Objects can now be picked up from further away
    
    The object to pick up is now decided on distance from the giant, not what entered the range detector last
    
    Fixed glitch where the last object in the detector could be picked up even if it was no longer in range
    
    Pickup detector now fetches the root of the collisions gameobject.
    
    Fixed glitch where anything inside the area that a villager wanted to build was hard to pick up due to the detector trying to pick up the area display.
    
    No more errors from the pick-up variable not being assigned to as if there is nothing to pick up, the variable will be filled by the directional light (As it is so far out of the way it won't intervine with anything, and is also impossible to pick up)

--- Project Giant/Assets/Scripts/PickUpDetect.cs
+++ Project Giant/Assets/Scripts/PickUpDetect.cs
@@ -6,23 +6,31 @@ public class PickUpDetect : MonoBehaviour
 {
 
     public GameObject pickUp;
+    private GameObject pickUpDummy;
     public GameObject giant;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pickUpDummy = GameObject.Find("Directional Light");
+        pickUp = pickUpDummy;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (pickUp == null)
+        {
+            pickUp = pickUpDummy;
+        }
     }
 
     public void OnTriggerEnter(Collider collision)
     {
-        //print("Collision entered");
-        pickUp = collision.gameObject;
+        if (pickUp == null && collision.gameObject != giant && (collision.GetComponent<Object>() != null || collision.GetComponent<Villager>() != null))
+        {
+            pickUp = collision.transform.root.gameObject;
+        }
+
     }
 
     public void GetPickUp()
@@ -44,5 +52,24 @@ public class PickUpDetect : MonoBehaviour
         giant.SendMessage("AttackFinished", SendMessageOptions.DontRequireReceiver);
         print("Object Attacked");
         pickUp.SendMessage("lifeDown", SendMessageOptions.DontRequireReceiver);
+    }
+
+    public void OnTriggerStay(Collider collision)
+    {
+        //print("Collision entered");
+        if (((Vector3.Distance(collision.transform.position, giant.transform.position) < Vector3.Distance(pickUp.transform.position, giant.transform.position)) || pickUp == null) && collision.gameObject != giant && (collision.GetComponent<Object>() != null || collision.GetComponent<Villager>() != null))
+        {
+
+            pickUp = collision.transform.root.gameObject;
+        }
+    }
+
+    public void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject == pickUp)
+        {
+            pickUp = pickUpDummy;
+        }
+
     }
 }
