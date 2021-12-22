52ec40aa52aaa8c9d98740b0b66de0cd1fa6c175
Oli <oli.taylor.2001@gmail.com>
2021-12-22 19:05:02 +0000

    Farms and trees
    Red Villagers can now build farms which grow wheat
    
    New Tree and seed models created

--- Project Giant/Assets/Scripts/Scaffolding.cs
+++ Project Giant/Assets/Scripts/Scaffolding.cs
@@ -6,10 +6,12 @@ public class Scaffolding : MonoBehaviour
 {
     public GameObject building;
     public GameObject villager;
+    public int newVillagers;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(StartBuild());
+        transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, Random.Range(-180, 180), transform.rotation.eulerAngles.z);
     }
 
     // Update is called once per frame
@@ -21,10 +23,13 @@ public class Scaffolding : MonoBehaviour
     private IEnumerator StartBuild()
     {
         yield return new WaitForSeconds(60f);
-        Instantiate(building, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity);
+        GameObject newBuild = Instantiate(building, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity);
+        newBuild.transform.rotation = gameObject.transform.rotation;
         yield return new WaitForSeconds(45f);
-        Instantiate(villager, new Vector3(transform.position.x + 0.5f, transform.position.y + 1, transform.position.z), Quaternion.identity);
-        Instantiate(villager, new Vector3(transform.position.x - 0.5f, transform.position.y + 1, transform.position.z), Quaternion.identity);
+        for (int i = 0; i < newVillagers; i++)
+        {
+            Instantiate(villager, new Vector3(transform.position.x + 0.5f, transform.position.y + 1, transform.position.z + (i / 5)), Quaternion.identity);
+        }
         Destroy(gameObject);
     }
 }
