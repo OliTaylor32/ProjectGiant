def90d494a815697b221b995a6e804c3347a9d6d
Oli <oli.taylor.2001@gmail.com>
2022-04-13 12:39:30 +0100

    Added encyclopedia
    Added articles for controls and game basics
    
    Need to un-comment steamworks code once ready to publish

--- Project Giant/Assets/Scripts/Title Screen/MainMenu.cs
+++ Project Giant/Assets/Scripts/Title Screen/MainMenu.cs
@@ -11,6 +11,10 @@ public class MainMenu : MonoBehaviour
     public TextMeshProUGUI[] freeplayModes;
     public GameObject freeplayLocation;
     public TextMeshProUGUI[] challenges;
+    public TextMeshProUGUI[] encyclopediaEntries;
+    public TextMeshProUGUI encyclopediaTxt;
+    private string[] articles;
+    public GameObject ePanel;
     public GameObject settings;
     public GameObject freeplayMapSS;
     public Image background;
@@ -21,41 +25,27 @@ public class MainMenu : MonoBehaviour
     public int menuLayer;
 
     public Fade fade;
+    public GameObject logo;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
-        if (selected == 0)
-        {
-            for (int i = 0; i < freeplayModes.Length; i++)
-            {
-                freeplayModes[i].gameObject.SetActive(false);
-            }
-            freeplayLocation.SetActive(false);
-            freeplayMapSS.SetActive(false);
-        }
-        if (selected == 1)
-        {
-            for (int i = 0; i < challenges.Length; i++)
-            {
-                challenges[i].gameObject.SetActive(false);
-            }
-        }
-        settings.SetActive(false);
-        if (Steamworks.SteamClient.AppId != 1903330)
-        {
-            try
-            {
-                Steamworks.SteamClient.Init(1903330);
-            }
-            catch (System.Exception)
-            {
-                print("Steamworks error");
-                throw;
-            }
-        }
 
-        print(Steamworks.SteamClient.Name);
+        settings.SetActive(false);
+        //if (Steamworks.SteamClient.AppId != 1903330)
+        //{
+        //    try
+        //    {
+        //        Steamworks.SteamClient.Init(1903330);
+        //    }
+        //    catch (System.Exception)
+        //    {
+        //        print("Steamworks error");
+        //        throw;
+        //    }
+        //}
+
+        //print(Steamworks.SteamClient.Name);
 
         //Top Menu
         selected = 0;
@@ -77,11 +67,24 @@ public class MainMenu : MonoBehaviour
 
         freeplayLocation.SetActive(false);
         freeplayMapSS.SetActive(false);
+
+        //Challenges Menu
         for (int i = 0; i < challenges.Length; i++)
         {
             challenges[i].gameObject.SetActive(false);
         }
+
+        //Encyclopedia Menu
+        for (int i = 0; i < encyclopediaEntries.Length; i++)
+        {
+            encyclopediaEntries[i].gameObject.SetActive(false);
+        }
+        encyclopediaTxt.gameObject.SetActive(false);
+        ePanel.SetActive(false);
+
+        //Settings Menu
         settings.SetActive(false);
+        SetEncyclopediaText();
     }
 
     // Update is called once per frame
@@ -123,7 +126,7 @@ public class MainMenu : MonoBehaviour
         {
             if (menuLayer == 0)
             {
-                if (selected == 3)
+                if (selected == 4)
                 {
                     Steamworks.SteamClient.Shutdown();
                     Application.Quit();
@@ -156,7 +159,7 @@ public class MainMenu : MonoBehaviour
                     }
                 }
 
-                if (selected == 2)
+                if (selected == 3)
                 {
                     Screen.fullScreen = !Screen.fullScreen;
                 }
@@ -196,13 +199,29 @@ public class MainMenu : MonoBehaviour
                 challenges[i].color = new Color(1f, 1f, 1f, 1f);
             }
         }
+        for (int i = 0; i < encyclopediaEntries.Length; i++)
+        {
+            encyclopediaEntries[i].color = new Color(0.5f, 0.5f, 0.5f, 1f);
+            if (i == selected2)
+            {
+                encyclopediaEntries[i].color = new Color(1f, 1f, 1f, 1f);
+            }
+            encyclopediaTxt.text = articles[selected2];
+        }
         yield return new WaitForSeconds(0.2f);
         cooldown = false;
     }
 
     private void SubMenu()
     {
-        background.sprite = backgroundSprites[1];
+        if (selected != 2)
+        {
+            background.sprite = backgroundSprites[1];
+        }
+        else
+        {
+            background.sprite = backgroundSprites[2];
+        }
         menuLayer = 1;
         if (selected == 0)
         {
@@ -222,6 +241,17 @@ public class MainMenu : MonoBehaviour
 
         }
         if (selected == 2)
+        {
+            for (int i = 0; i < encyclopediaEntries.Length; i++)
+            {
+                encyclopediaEntries[i].gameObject.SetActive(true);
+            }
+            encyclopediaTxt.gameObject.SetActive(true);
+            logo.SetActive(false);
+            ePanel.SetActive(true);
+
+        }
+        if (selected == 3)
         {
             settings.SetActive(true);
         }
@@ -251,6 +281,16 @@ public class MainMenu : MonoBehaviour
                 challenges[i].gameObject.SetActive(false);
             }
         }
+        if (selected == 2)
+        {
+            for (int i = 0; i < encyclopediaEntries.Length; i++)
+            {
+                encyclopediaEntries[i].gameObject.SetActive(false);
+            }
+            encyclopediaTxt.gameObject.SetActive(false);
+            logo.SetActive(true);
+            ePanel.SetActive(false);
+        }
         settings.SetActive(false);
     }
 
@@ -259,5 +299,17 @@ public class MainMenu : MonoBehaviour
         fade.StartFadeOut();
         yield return new WaitForSeconds(2f);
         Application.LoadLevel(level);
+    }
+
+    private void SetEncyclopediaText()
+    {
+        articles = new string[6];
+        articles[0] = "Controls (Gamepad/Keyboard):" + "\n" + "Look around: RS or Mouse" + "\n" + "Movement: LS or WASD" + "\n" + "Pick Up / Drop Objects: A or Space" + "\n" + "Attack (Must grow via tears): X or X key" + "\n" + "Camera Zoom: D-Pad or Scroll Wheel";
+        articles[1] = "Welcome to The Giant of Torridge Island! This article will go over the basics of the game and your abilities as the Giant." + "\n" + "The Goal of the game is to shape the island to your liking. As the Giant you have the power to pick up anything on the island (as long as you grow big enough). In order to grow you need to interact with the villagers of the island. The villagers will give out stars for good deeds and tears for bad ones." + "\n" + "Good deeds include completing requests from the villagers such as collecting materials or making space for new buildings." + "\n" + "Some bad deeds include hurting the villagers or destroying their buildings" + "\n" + "The deeds you complete will then be collected to create a villager score at the end of the day. The other score you are given is a nature score." + "\n" + "Much like the villager score, the nature score can be changed via good and bad deeds towards the enviroment. Good deeds include planting trees and breeding animals, while bad deeds include chopping down trees and hunting animals";
+        articles[2] = "Trees and Stones";
+        articles[3] = "Villagers";
+        articles[4] = "Wildlife";
+        articles[5] = "Farms";
+        encyclopediaTxt.text = articles[0];
     }
 }
